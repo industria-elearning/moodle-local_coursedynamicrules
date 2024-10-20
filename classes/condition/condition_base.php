819f6853d741e52ee7db0378d192cf918820ba67
wilber-buendata <wilber@buendata.com>
2024-10-19 19:55:17 -0500

    add abstract class condition_base to help with dynamic form generation

---
+++ classes/condition/condition_base.php
@@ -0,0 +1,121 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace local_coursedynamicrules\condition;
+
+use stdClass;
+
+/**
+ * Class condition_base
+ *
+ * @package    local_coursedynamicrules
+ * @copyright  2024 Industria Elearning <info@industriaelearning.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+abstract class condition_base {
+
+    /** @var string type of the element, should be overridden by each condition type */
+    protected $type;
+
+    /** @var condition_form */
+    protected $conditionform;
+
+    /** @var  \stdClass */
+    protected $condition;
+
+    /**
+     * constructor
+     */
+    public function __construct() {
+    }
+
+    /**
+     * Displays the form for editing an condition
+     *
+     * this function only can used after the call of build_editform()
+     */
+    public function show_editform() {
+        $this->conditionform->display();
+    }
+
+    /**
+     * Checks if the editing form was cancelled
+     *
+     * @return bool
+     */
+    public function is_cancelled() {
+        return $this->conditionform->is_cancelled();
+    }
+
+    /**
+     * Gets submitted data from the edit form and saves it in $this->condition
+     *
+     * @return bool
+     */
+    public function get_data() {
+        if ($this->condition !== null) {
+            return true;
+        }
+        if ($this->condition = $this->conditionform->get_data()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Set the condition data (to be used by data generators).
+     *
+     * @param stdClass $conditiondata the condition data to set
+     */
+    public function set_data($conditiondata) {
+        $this->condition = $conditiondata;
+    }
+
+    /**
+     * Creates and returns an instance of the form for editing the condition
+     *
+     * @param stdClass $condition object that represents the condition on the database
+     */
+    abstract public function build_editform($condition);
+
+    /**
+     * Saves the condition after it has been edited (or created)
+     */
+    abstract public function save_condition();
+
+    /**
+     * Returns the formatted name of the condition for the complete form or response view
+     *
+     * @param stdClass $condition
+     * @param bool $withpostfix
+     * @return string
+     */
+    public function get_display_name($condition, $withpostfix = true) {
+        return format_text($condition->name, FORMAT_HTML, ['noclean' => true, 'para' => false]) .
+                ($withpostfix ? $this->get_display_name_postfix($condition) : '');
+    }
+
+    /**
+     * Returns the postfix to be appended to the display name that is based on other settings
+     *
+     * @param stdClass $condition
+     * @return string
+     */
+    public function get_display_name_postfix($condition) {
+        return '';
+    }
+
+}
