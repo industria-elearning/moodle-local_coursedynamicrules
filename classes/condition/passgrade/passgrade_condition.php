b05fed73aa6f3d9bc316ad93974620b38f859aff
wilber-buendata <wilber@buendata.com>
2024-10-20 21:14:55 -0500

    update build form to retrieve form params

--- classes/condition/passgrade/passgrade_condition.php
+++ classes/condition/passgrade/passgrade_condition.php
@@ -33,14 +33,32 @@ class passgrade_condition extends condition_base {
     /**
      * Creates and returns an instance of the form for editing the item
      *
-     * @param stdClass $condition object that represents the condition on the database
+     * @param mixed $action the action attribute for the form. If empty defaults to auto detect the
+     *              current url. If a moodle_url object then outputs params as hidden variables.
+     * @param mixed $customdata if your form defintion method needs access to data such as $course
+     *              $cm, etc. to construct the form definition then pass it in this array. You can
+     *              use globals for somethings.
+     * @param string $method if you set this to anything other than 'post' then _GET and _POST will
+     *               be merged and used as incoming data to the form.
+     * @param string $target target frame for form submission. You will rarely use this. Don't use
+     *               it if you don't need to as the target attribute is deprecated in xhtml strict.
+     * @param mixed $attributes you can pass a string of html attributes here or an array.
+     *               Special attribute 'data-random-ids' will randomise generated elements ids. This
+     *               is necessary when there are several forms on the same page.
+     *               Special attribute 'data-double-submit-protection' set to 'off' will turn off
+     *               double-submit protection JavaScript - this may be necessary if your form sends
+     *               downloadable files in response to a submit button, and can't call
+     *               \core_form\util::form_download_complete();
+     * @param bool $editable
+     * @param array $ajaxformdata Forms submitted via ajax, must pass their data here, instead of relying on _GET and _POST.
      */
-    public function build_editform($condition) {
+    public function build_editform($action=null, $customdata=null, $method='post', $target='', $attributes=null, $editable=true,
+    $ajaxformdata=null) {
         global $DB, $CFG;
         $customdata = [];
         // $editconditionurl = new moodle_url('/local/coursedynamicrules/editcondition.php', []);
 
-        $this->conditionform = new passgrade_form('editcondition.php', $customdata);
+        $this->conditionform = new passgrade_form($action, $customdata, $method, $target, $attributes, $editable, $ajaxformdata);
 
     }
 
