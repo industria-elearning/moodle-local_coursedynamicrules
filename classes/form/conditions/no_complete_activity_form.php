0146b2cbfdec765cb9dd5aea97808ab9c139b22d
wilber-buendata <wilber@buendata.com>
2024-11-17 20:12:54 -0500

    add no_complete_activity_form

---
+++ classes/form/conditions/no_complete_activity_form.php
@@ -0,0 +1,76 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace local_coursedynamicrules\form\conditions;
+
+/**
+ * Class no_complete_activity_form
+ *
+ * @package    local_coursedynamicrules
+ * @copyright  2024 Industria Elearning <info@industriaelearning.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class no_complete_activity_form extends condition_form {
+    /** @var string type of condition */
+    protected $type = "no_complete_activity";
+    /**
+     * Form definition
+     *
+     * @return void
+     */
+    public function definition() {
+        $mform = $this->_form;
+        $customdata = $this->_customdata;
+        $courseid = $customdata['courseid'];
+        $ruleid = $customdata['ruleid'];
+
+        $modinfo = get_fast_modinfo($courseid);
+        $cms = $modinfo->get_cms();
+        $options = [];
+        foreach ($cms as $cm) {
+            if ($cm->completion == COMPLETION_TRACKING_AUTOMATIC) {
+                $options[$cm->id] = ucfirst($cm->modname) . " - " . $cm->name;
+            }
+        }
+
+        $attributes = [
+            'multiple' => false,
+            'noselectionstring' => get_string('allcourseactivitymodules', 'local_coursedynamicrules'),
+        ];
+        $mform->addElement(
+            'autocomplete',
+            'coursemodule',
+            get_string('searchcourseactivitymodules',
+            'local_coursedynamicrules'),
+            $options,
+            $attributes
+        );
+        $mform->setType('coursemodule', PARAM_INT);
+
+        $mform->addElement(
+            'date_time_selector',
+            'expectedcompletiondate',
+            get_string('expectedcompletiondate', 'local_coursedynamicrules')
+        );
+
+        $mform->addElement('hidden', 'type', $this->type);
+        $mform->addElement('hidden', 'ruleid', $ruleid);
+        $mform->setType('type', PARAM_ALPHA);
+        $mform->setType('ruleid', PARAM_INT);
+
+        parent::definition();
+    }
+}
