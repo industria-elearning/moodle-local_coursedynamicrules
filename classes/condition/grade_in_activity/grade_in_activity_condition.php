18f907373348a14dc69f72f80ce0b8156ff638b3
wilber-buendata <wilber@buendata.com>
2024-11-30 14:57:30 -0500

    add initial grade_in_activity_condition class

---
+++ classes/condition/grade_in_activity/grade_in_activity_condition.php
@@ -0,0 +1,121 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace local_coursedynamicrules\condition\grade_in_activity;
+
+use local_coursedynamicrules\core\condition;
+use local_coursedynamicrules\form\conditions\grade_in_activity_form;
+
+/**
+ * Class grade_in_activity_condition
+ *
+ * @package    local_coursedynamicrules
+ * @copyright  2024 Industria Elearning <info@industriaelearning.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class grade_in_activity_condition extends condition {
+    /** @var string type of condition */
+    protected $type = "grade_in_activity";
+
+    /**
+     * Evaluate the condition and return true if the condition is met
+     * @param object $context Context of the rule
+     * @return bool
+     */
+    public function evaluate($context) {
+        return true;
+    }
+
+    /**
+     * Returns the header of the condition to visualization
+     *
+     * @return string
+     */
+    public function get_header() {
+        return get_string('grade_in_activity', 'local_coursedynamicrules');
+    }
+
+    /**
+     * Returns the description of the condition to visualization
+     *
+     * @return string
+     */
+    public function get_description() {
+        $courseid = $this->courseid;
+        $cmid = $this->params->cmid;
+        $modinfo = get_fast_modinfo($courseid);
+        $cms = $modinfo->get_cms();
+        $cminfo = $cms[$cmid];
+
+        if (!$cminfo) {
+            return '';
+        }
+        return get_string(
+            'grade_in_activity_description',
+            'local_coursedynamicrules', ucfirst($cminfo->modname) . " - " . $cminfo->name
+        );
+    }
+
+    /**
+     * Creates and returns an instance of the form for editing the item
+     *
+     * @param mixed $action the action attribute for the form. If empty defaults to auto detect the
+     *              current url. If a moodle_url object then outputs params as hidden variables.
+     * @param mixed $customdata if your form defintion method needs access to data such as $course
+     *              $cm, etc. to construct the form definition then pass it in this array. You can
+     *              use globals for somethings.
+     * @param string $method if you set this to anything other than 'post' then _GET and _POST will
+     *               be merged and used as incoming data to the form.
+     * @param string $target target frame for form submission. You will rarely use this. Don't use
+     *               it if you don't need to as the target attribute is deprecated in xhtml strict.
+     * @param mixed $attributes you can pass a string of html attributes here or an array.
+     *               Special attribute 'data-random-ids' will randomise generated elements ids. This
+     *               is necessary when there are several forms on the same page.
+     *               Special attribute 'data-double-submit-protection' set to 'off' will turn off
+     *               double-submit protection JavaScript - this may be necessary if your form sends
+     *               downloadable files in response to a submit button, and can't call
+     *               \core_form\util::form_download_complete();
+     * @param bool $editable
+     * @param array $ajaxformdata Forms submitted via ajax, must pass their data here, instead of relying on _GET and _POST.
+     */
+    public function build_editform(
+        $action=null,
+        $customdata=null,
+        $method='post',
+        $target='',
+        $attributes=null,
+        $editable=true,
+        $ajaxformdata=null
+    ) {
+        $this->conditionform = new grade_in_activity_form(
+            $action,
+            $customdata,
+            $method,
+            $target,
+            $attributes,
+            $editable,
+            $ajaxformdata
+        );
+    }
+
+    /**
+     * Saves the condition after it has been edited (or created)
+     * @param object $formdata
+     */
+    public function save_condition($formdata) {
+
+    }
+}
