b1b0d68fae90ff5f3b5b1c163d9e87970765d418
wilber-buendata <wilber@buendata.com>
2024-11-30 11:22:49 -0500

    standarize lang strings

--- editrule.php
+++ editrule.php
@@ -65,7 +65,7 @@ if ($ruleform->is_cancelled()) {
         $DB->insert_record('cdr_rule', $data);
         redirect(
             $rulesurl,
-            get_string('rule:addedsuccessfully', 'local_coursedynamicrules'),
+            get_string('ruleaddedsuccessfully', 'local_coursedynamicrules'),
             null,
             \core\output\notification::NOTIFY_SUCCESS
         );
@@ -73,7 +73,7 @@ if ($ruleform->is_cancelled()) {
         $DB->update_record('cdr_rule', $data);
         redirect(
             $rulesurl,
-            get_string('rule:updatedsuccessfully', 'local_coursedynamicrules'),
+            get_string('ruleupdatedsuccessfully', 'local_coursedynamicrules'),
             null,
             \core\output\notification::NOTIFY_SUCCESS
         );
