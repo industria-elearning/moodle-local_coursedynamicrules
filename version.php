2d703bf780c6330468dcf609ebeb38634ddae681
wilber-buendata <wilber@buendata.com>
2024-12-02 18:04:35 -0500

    update to version 2024120200

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_coursedynamicrules';
 $plugin->release = '1.1.1';
-$plugin->version = 2024122300;
+$plugin->version = 2024120200;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
