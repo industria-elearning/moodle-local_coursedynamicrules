07bc4f3bd38526dc954de7a8af6d369b954253c3
wilber-buendata <wilber@buendata.com>
2024-12-24 12:34:11 -0500

    update grade activity description and increment version to 1.2.0

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_coursedynamicrules';
-$plugin->release = '1.1.1';
-$plugin->version = 2024121400;
+$plugin->release = '1.2.0';
+$plugin->version = 2024122403;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
