72460b332fcd1fe8ce39c9a72dcc7a54505029fb
wilber-buendata <wilber@buendata.com>
2024-12-24 07:59:42 -0500

    refactor grade item handling in dynamic form: reset and update logic improvements

--- amd/src/grade_in_activity_form.js
+++ amd/src/grade_in_activity_form.js
@@ -60,78 +60,8 @@ function handleLoadForm(dynamicForm) {
     dynamicForm.load({courseid: courseId})
         .then(() => {
             attachCourseModuleChangeListener(dynamicForm);
-
-            // Handle gradeitems dynamics conditions.
-            document.querySelector('[name=gradeitems]').value = JSON.stringify({});
-
-            const cmId = document.querySelector('[name=coursemodule]').value;
-
-            document.querySelector('[name=cmid]').value = cmId;
-            const cmConditionInputs = document.querySelectorAll(`[data-cmid='${cmId}']`);
-
-            cmConditionInputs.forEach((input) => {
-                const gradeItems = document.querySelector('[name=gradeitems]').value;
-                const gradeItemsObject = JSON.parse(gradeItems);
-
-                const condition = input.dataset.condition;
-                const gradeItem = input.dataset.gradeitem;
-                const value = input.value;
-                const gradeItemKey = `${condition}_${gradeItem}`;
-                gradeItemsObject[gradeItemKey] = {
-                    gradeitem: gradeItem,
-                    condition: condition,
-                    value: value,
-                };
-
-                document.querySelector('[name=gradeitems]').value = JSON.stringify(gradeItemsObject);
-
-                input.addEventListener('change', (e) => {
-                    const gradeItems = document.querySelector('[name=gradeitems]').value;
-                    const gradeItemsObject = JSON.parse(gradeItems);
-
-                    const condition = e.target.dataset.condition;
-                    const gradeItem = e.target.dataset.gradeitem;
-                    const value = e.target.value;
-                    const gradeItemKey = `${condition}_${gradeItem}`;
-
-                    gradeItemsObject[gradeItemKey] = {
-                        gradeitem: gradeItem,
-                        condition: condition,
-                        value: value,
-                    };
-
-                    document.querySelector('[name=gradeitems]').value = JSON.stringify(gradeItemsObject);
-                });
-            });
-
-            // Handle inputs validation and submission.
-            const gradeInActivityForm = document.getElementById('grade_in_activity_form');
-            const dynamicGradeInActivityForm = document.getElementById('dynamic_grade_in_activity_form');
-            gradeInActivityForm.addEventListener('submit', (e) => {
-                e.preventDefault();
-                const cmId = document.querySelector('[name=coursemodule]').value;
-                const cmConditionInputs = document.querySelectorAll(`[data-cmid='${cmId}']`);
-
-                cmConditionInputs.forEach((input) => {
-                    // eslint-disable-next-line no-console
-                    console.log(input.value);
-
-                    if (input.value > 4) {
-                        const id = input.id;
-                        const invalidFeedback = document.querySelector(`.invalid-feedback#${id}`);
-                        invalidFeedback.textContent = 'The grade must be between 0 and 4.';
-                        input.setCustomValidity('The grade must be between 0 and 4.');
-                    }
-                });
-
-                dynamicGradeInActivityForm.classList.add('was-validated');
-
-                if (dynamicGradeInActivityForm.checkValidity()) {
-                    gradeInActivityForm.submit();
-                    return;
-                }
-            });
-
+            resetGradeItems();
+            updateGradeItems(dynamicForm);
             return loadPromise.resolve();
         })
         .catch(Notification.exception);
@@ -171,55 +101,58 @@ function handleCourseModuleChange(dynamicForm, courseModuleValue) {
     dynamicForm.load({coursemodule: courseModuleValue, courseid: courseId})
     .then(() => {
         attachCourseModuleChangeListener(dynamicForm);
-
-        document.querySelector('[name=cmid]').value = courseModuleValue;
-
-        document.querySelector('[name=gradeitems]').value = JSON.stringify({});
-
-        const cmId = document.querySelector('[name=coursemodule]').value;
-        const cmConditionInputs = document.querySelectorAll(`[data-cmid='${cmId}']`);
-
-        cmConditionInputs.forEach((input) => {
-            const gradeItems = document.querySelector('[name=gradeitems]').value;
-            const gradeItemsObject = JSON.parse(gradeItems);
-
-            const condition = input.dataset.condition;
-            const gradeItem = input.dataset.gradeitem;
-            const value = input.value;
-            const disabled = input.getAttribute('disabled') === 'disabled';
-            const gradeItemKey = `${condition}_${gradeItem}`;
-            gradeItemsObject[gradeItemKey] = {
-                gradeitem: gradeItem,
-                condition: condition,
-                value: value,
-                disabled: disabled,
-            };
-
-            document.querySelector('[name=gradeitems]').value = JSON.stringify(gradeItemsObject);
-
-            input.addEventListener('change', (e) => {
-                const gradeItems = document.querySelector('[name=gradeitems]').value;
-                const gradeItemsObject = JSON.parse(gradeItems);
-
-                const condition = e.target.dataset.condition;
-                const gradeItem = e.target.dataset.gradeitem;
-                const value = e.target.value;
-                const gradeItemKey = `${condition}_${gradeItem}`;
-
-                const disabled = e.target.getAttribute('disabled') === 'disabled';
-
-                gradeItemsObject[gradeItemKey] = {
-                    gradeitem: gradeItem,
-                    condition: condition,
-                    value: value,
-                    disabled: disabled,
-                };
-
-                document.querySelector('[name=gradeitems]').value = JSON.stringify(gradeItemsObject);
-            });
-        });
+        resetGradeItems();
+        updateGradeItems(dynamicForm);
         return updatePromise.resolve();
     })
     .catch(Notification.exception);
+}
+
+/**
+ * Resets the grade items field.
+ */
+function resetGradeItems() {
+    document.querySelector('[name=gradeitems]').value = JSON.stringify({});
+}
+
+/**
+ * Updates the grade items based on the current form state.
+ *
+ */
+function updateGradeItems() {
+    const cmId = document.querySelector('[name=coursemodule]').value;
+    document.querySelector('[name=cmid]').value = cmId;
+    const cmConditionInputs = document.querySelectorAll(`[data-cmid='${cmId}']`);
+
+    cmConditionInputs.forEach((input) => {
+        updateGradeItem(input);
+
+        input.addEventListener('change', (e) => {
+            updateGradeItem(e.target);
+        });
+    });
+}
+
+/**
+ * Updates a single grade item based on the input element.
+ *
+ * @param {HTMLElement} input The input element.
+ */
+function updateGradeItem(input) {
+    const gradeItems = document.querySelector('[name=gradeitems]').value;
+    const gradeItemsObject = JSON.parse(gradeItems);
+
+    const condition = input.dataset.condition;
+    const gradeItem = input.dataset.gradeitem;
+    const value = input.value;
+    const gradeItemKey = `${condition}_${gradeItem}`;
+
+    gradeItemsObject[gradeItemKey] = {
+        gradeitem: gradeItem,
+        condition: condition,
+        value: value,
+    };
+
+    document.querySelector('[name=gradeitems]').value = JSON.stringify(gradeItemsObject);
 }
 
