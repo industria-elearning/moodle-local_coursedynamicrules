2915cbe42d06ad5fbcd91eebb7fd2c9a19cb7abf
wilber-buendata <wilber@buendata.com>
2024-12-27 15:13:26 -0500

    bump version number to 2024122700

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_coursedynamicrules';
 $plugin->release = '1.2.0';
-$plugin->version = 2024122600;
+$plugin->version = 2024122700;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
