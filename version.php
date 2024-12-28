350bff5e08a07a40a18d25769806882c5025e6e9
wilber-buendata <wilber@buendata.com>
2024-12-28 17:31:34 -0500

    bump version to 1.2.1

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_coursedynamicrules';
-$plugin->release = '1.2.0';
+$plugin->release = '1.2.1';
 $plugin->version = 2024122800;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
