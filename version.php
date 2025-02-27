b59d1b7e9608324e20e77c8ec8b788e7d592768b
wilber-buendata <wilber@buendata.com>
2025-02-27 09:45:47 -0500

    update database schema to add lastexecutiontime field to cdr_condition and cdr_action tables

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_coursedynamicrules';
 $plugin->release = '1.4.0';
-$plugin->version = 2025020403;
+$plugin->version = 2025022601;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
