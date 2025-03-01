968bfebe1cc70ec0fe47784d650270e3a8bf28e8
wilber-buendata <wilber@buendata.com>
2025-03-01 11:44:22 -0500

    fix phpcs error on generator lib

--- tests/generator/lib.php
+++ tests/generator/lib.php
@@ -1,6 +1,4 @@
 <?php
-
-use local_coursedynamicrules\core\rule;
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -15,6 +13,8 @@ use local_coursedynamicrules\core\rule;
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+use local_coursedynamicrules\core\rule;
 
 /**
  * Data generator class
