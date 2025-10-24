dbc2c77d2f7831bdac9455802338cdc4d2109e86
wilber-buendata <wilber@buendata.com>
2025-10-24 15:39:27 -0500

    chore: bump plugin version to 2025102403

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_coursedynamicrules';
 $plugin->release = '1.6.0';
-$plugin->version = 2025102400;
+$plugin->version = 2025102403;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
